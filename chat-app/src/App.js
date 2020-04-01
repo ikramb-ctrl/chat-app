84d8ec2dc2350760607906a111d5db381a2c4bcd
ikramb-ctrl <b_zakia@homail.com>
2020-04-01 17:05:37 +0200

    commit dossier chat-app

---
+++ chat-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Contact from './Contact.js';
+
+function App() {
+  return (
+    <div>
+      <Contact name="Joann Garza" avatar="https://randomuser.me/api/portraits/women/76.jpg" online={true} />
+    
+    </div>
+  );
+}
+
+export default App;
