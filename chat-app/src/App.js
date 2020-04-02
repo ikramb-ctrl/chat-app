ec2e136b46ed84e1a3e2fab5056a2c0bdd5f07e7
ikramb-ctrl <59873336+ikramb-ctrl@users.noreply.github.com>
2020-04-02 11:16:57 +0200

    Update App.js

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div>
       <Contact name="Joann Garza" avatar="https://randomuser.me/api/portraits/women/76.jpg" online={true} />
+      <Contact name="Addison Pena" avatar="https://randomuser.me/api/portraits/women/4.jpg" online={false} />
+      <Contact name="Adisson Fuller" avatar="https://randomuser.me/api/portraits/women/26.jpg" online={true} />
     
     </div>
   );
