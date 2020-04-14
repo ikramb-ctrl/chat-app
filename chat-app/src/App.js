67711738b3d2adb9352a1a72a41d67a5a85eb123
ikramb-ctrl <59873336+ikramb-ctrl@users.noreply.github.com>
2020-04-14 11:56:01 +0200

    Update App.js

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Contact from './components/Contact.js';
+import ContactList from './components/ContactList.js';
 
 function App() {
   return (
     <div>
-      <Contact name="Joann Garza" avatar="https://randomuser.me/api/portraits/women/76.jpg" online={true} />
-      <Contact name="Addison Pena" avatar="https://randomuser.me/api/portraits/women/4.jpg" online={false} />
-      <Contact name="Adisson Fuller" avatar="https://randomuser.me/api/portraits/women/26.jpg" online={true} />
-    
+      <ContactList />
     </div>
   );
 }
