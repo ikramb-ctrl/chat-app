5f44340ec2bfd45277edabae979c4fb387166061
ikramb-ctrl <59873336+ikramb-ctrl@users.noreply.github.com>
2020-04-14 11:51:33 +0200

    Update Contact.js

--- chat-app/src/components/Contact.js
+++ chat-app/src/components/Contact.js
@@ -1,29 +1,50 @@
-import React from 'react';
-import './Contact.css';
-import PropTypes from 'prop-types';
+import React from "react";
+import "./Contact.css";
+import PropTypes from "prop-types";
 
-function Contact (props) {
-  return (
-    <div className='Contact'>
-      <img className='avatar' src={props.avatar}
-      />
-      <div>
-        <h4 className='name'>{props.name}</h4>
-        
-        <div className='status'>
-          <img className={props.online ? 'status-online' : 'status-offline'}
-          />
-          <p className='status-text'>{props.online ? 'online' : 'offline'}</p>
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: props.online
+    };
+  }
+
+  render() {
+    return (
+      <div className="Contact">
+        <img className="avatar" src={this.props.avatar} />
+        <div>
+          <h4 className="name">{this.props.name}</h4>
+          <div
+            className="status"
+            onClick={event => {
+              const newStatus = !this.state.online;
+              this.setState({ online: newStatus });
+            }}
+          >
+            {this.state.online ? (
+              <div>
+                <div className="status-online" />
+                <p className="status-text">online</p>
+              </div>
+            ) : (
+              <div>
+                <div className="status-offline" />
+                <p className="status-text">offline</p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 Contact.propTypes = {
   avatar: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  online: PropTypes.bool.isRequired,
+  online: PropTypes.bool.isRequired
 };
 
 export default Contact;
